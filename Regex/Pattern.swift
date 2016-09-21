ed9de67a4f185de46f3639992204beddee13485c
Keiji Suzuki <zuki.ebetsu@gmail.com>
2016-09-21 16:22:09 +0900

    アクセス修飾子をpublicに戻した

--- Regex/Pattern.swift
+++ Regex/Pattern.swift
@@ -8,11 +8,11 @@
 
 import Foundation
 
-open class Pattern {
-    open static let email = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}+$"
-    open static let ipv4 = "(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})"
+public class Pattern {
+    public static let email = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}+$"
+    public static let ipv4 = "(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})"
 
-    open static func url(_ scheme: String? = "https?") -> String {
+    public static func url(_ scheme: String? = "https?") -> String {
         return "\(scheme != nil ? scheme! : "[a-z]+")://[a-zA-Z0-9\\.]+\\/[\\w-\\.\\/?%&=]*"
     }
 }
